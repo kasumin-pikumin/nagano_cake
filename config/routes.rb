121ca2bbfbb8dc41d6c1db67a77d419476edc5de
EC2 Default User <ec2-user@ip-172-31-0-41.ap-northeast-1.compute.internal>
2023-03-11 08:17:42 +0000

    [Add]テーブル、カラムの作成

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     sessions: 'public/sessions'
   }
 
-  get '/top' => 'homes#top'
+  root to: "public/homes#top"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
