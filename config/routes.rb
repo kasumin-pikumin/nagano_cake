681b8848232c5b0a9a223034f7d872f6513eb26f
EC2 Default User <ec2-user@ip-172-31-0-41.ap-northeast-1.compute.internal>
2023-03-21 00:51:26 +0000

    [Add]会員一覧、詳細画面(admin)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :admin, skip: [:registrations, :passwords] , controllers: {
     sessions: "admin/sessions"
   }
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root 'homes#top'
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :customers, only: [:index, :show, :edit]
 
   end
 
