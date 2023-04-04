cf0deccb99016e6d9804ee57f4d6993bb76d82e0
EC2 Default User <ec2-user@ip-172-31-0-41.ap-northeast-1.compute.internal>
2023-04-04 23:14:38 +0000

    [Add]enumの導入

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,12 +1,18 @@
 class Public::CartItemsController < ApplicationController
-  def index
-    @cart_items = current_customer.cart_items
-  end
 
   def create
-    @cart_item.customer_id = current_customer.id
-    @cart_item.save!
+    @cart_item = CartItem.new(cart_items_params)
+    @cart_item.customer_id = current_customer
+    @cart_item.item_id = params[:cart_item][:item_id]
+    # binding.pry
+    @cart_item.save
     redirect_to cart_items_path
+  end
+
+
+  def index
+    @cart_items = current_customer.cart_item.all
+    @item = Item.find(params[:id])
   end
 
   def destroy
