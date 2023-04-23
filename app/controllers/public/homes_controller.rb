31e09aaef2924b4ea8809ff5075acff6e294d8ef
EC2 Default User <ec2-user@ip-172-31-0-41.ap-northeast-1.compute.internal>
2023-04-23 04:59:42 +0000

    完成（仮）

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,5 +2,6 @@ class Public::HomesController < ApplicationController
   def top
     @genres = Genre.all
     @items = Item.all
+    @items = Item.all.order(created_at: :desc) .limit(4)
   end
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,5 +1,6 @@
 class Public::ItemsController < ApplicationController
   def index
+    @genres = Genre.all
     @items = Item.all
   end
 
